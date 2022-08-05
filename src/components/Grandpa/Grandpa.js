6fd0ae0c0b9668be6df317552c6fbc9732f111b9
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-08-05 23:56:35 +0600

    contextapi

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Anty from '../Anty/Anty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css';
 
-
+export const RingContext = createContext('Ring');
 const Grandpa = () => {
     const [house, setHouse] = useState(1);
     const ornaments = 'Diamond Ring';
@@ -14,16 +14,18 @@ const Grandpa = () => {
         setHouse(newHOuseCount);
     }
     return (
-        <div className='grandPa' >
-            <h4>Grandpa</h4>
-            <button onClick={handleHouseCount}>Buy A House</button>
-            <p>House:{house}</p>
-            <section style={{ display: 'flex' }}>
-                <Father house={house} ornaments={ornaments}></Father>
-                <Uncle house={house}></Uncle>
-                <Anty house={house}></Anty>
-            </section>
-        </div>
+        <RingContext.Provider value='alur ring'>
+            <div className='grandPa' >
+                <h4>Grandpa</h4>
+                <button onClick={handleHouseCount}>Buy A House</button>
+                <p>House:{house}</p>
+                <section style={{ display: 'flex' }}>
+                    <Father house={house} ornaments={ornaments}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Anty house={house}></Anty>
+                </section>
+            </div>
+        </RingContext.Provider>
     );
 };
 
--- src/components/Special/Special.js
+++ src/components/Special/Special.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ ornaments }) => {
+    const ring = useContext(RingContext);
     return (
         <div>
             <h3>Special</h3>
-            <p>ornaments: {ornaments}</p>
+            <p>ornaments: {ring}</p>
         </div>
     );
 };
